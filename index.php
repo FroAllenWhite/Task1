084dbc13cc07ce84171a2551cc8de2ef322325f6
FroAllenWhite <118559993+FroAllenWhite@users.noreply.github.com>
2023-07-15 19:17:35 +0400

    job start

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="ru">
+
+<head>
+    <title>Аутентификация</title>
+    <style>
+        <?php include 'style.css'; ?>
+    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container bg-light text-dark rounded" id="form">
+    <a href="#" class="navbar-brand">Авторизация</a>
+    <hr>
+    <form action="login.php">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <button class="btn btn-primary">Войти</button>
+    </form>
+</div>
+</body>
+
+</html>
